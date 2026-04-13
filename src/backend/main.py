27fd6c986a5ab8dbd462f36e0ed0f6dc8d322b2c
maksym <kapkomaxim@gmail.com>
2026-04-13 18:02:37 +0300

    Add copytrade, page + logic, database changes for copytrade, fix for calculating volumes

--- src/backend/main.py
+++ src/backend/main.py
@@ -159,4 +159,4 @@ async def websocket_endpoint(websocket: WebSocket):
                 asyncio.create_task(ws_listener.unsubscribe(sub_id))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, port=8001)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
